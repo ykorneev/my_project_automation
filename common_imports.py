bcb3c942939a8e78ffc3b518422e8ac96aab61c7
ykorneev <yura.korneev.25@gmail.com>
2025-03-13 18:27:00 +0100

    Удалил common_imports.py из репозитория и добавил в .gitignore

--- common_imports.py
+++
@@ -1,9 +0,0 @@
-import time
-import pytest
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-import selenium.webdriver.support
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.keys import Keys
-from selenium.common.exceptions import NoSuchElementException
