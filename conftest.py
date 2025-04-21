86a8614db804fd616c80b2edd3daf027c3632a2d
ykorneev <yura.korneev.25@gmail.com>
2025-04-21 16:04:19 +0200

    update common_imports

--- conftest.py
+++ conftest.py
@@ -2,9 +2,11 @@ import pytest
 from selenium import webdriver
 import allure
 
+BASE_URL = "https://www.demoblaze.com/"
+
 @pytest.fixture(scope="function")  # Запуск браузера перед каждым тестом
 def driver():
     driver = webdriver.Chrome()  # Открытие браузера
-    driver.get("https://www.demoblaze.com/")  # Переход на главную страницу
+    driver.get(BASE_URL)  # Переход на главную страницу
     yield driver
     driver.quit()  # Закрытие браузера после каждого теста
